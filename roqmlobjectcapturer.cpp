a33992fabbb2eaa32b314bc385b8aafbd57160ab
uNmAnNeR <ubivaza@gmail.com>
2014-12-02 14:17:40 +0300

    - исправлена ошибка захвата изображения с внутренних объектов сцены

--- roqmlobjectcapturer.cpp
+++ roqmlobjectcapturer.cpp
@@ -20,35 +20,13 @@ QImage ROQMLObjectCapturer::getImage(const QString& itemFile, const QString& ite
     if (!sourceItem || !_engine)
         return QImage();
 
-
-    QImage img(sourceItem->boundingRect().toRect().size(), QImage::Format_RGB32);
-    QPainter painter(&img);
-    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing |
-                           QPainter::SmoothPixmapTransform | QPainter::HighQualityAntialiasing);
-
-
-
     QDeclarativeComponent component(_engine, QUrl(itemFile));
-
-    //        TODO
-    //        if (!_component->isLoading()) {
-    //            continueExecute();
-    //        } else {
-    //            connect(_component, SIGNAL(statusChanged(QDeclarativeComponent::Status)), this, SLOT(continueExecute()));
-    //        }
-
-    while(component.isLoading()) {}
-
-
     QDeclarativeItem * item = qobject_cast<QDeclarativeItem *>(component.create(QDeclarativeEngine::contextForObject(sourceItem)));
 
-    if (!item) {
-        qDebug() << "BAD ITEM:" << itemFile;
+    if (!item)
         return QImage();
-    }
 
-    //        sourceItem->
-
+    // set item properties
     auto propIt = itemProps.constBegin();
     while (propIt != itemProps.constEnd()) {
         item->setProperty(propIt.key().toAscii(), propIt.value());
@@ -56,10 +34,32 @@ QImage ROQMLObjectCapturer::getImage(const QString& itemFile, const QString& ite
     }
 
 
+    QRectF targetRect = sourceItem->boundingRect();
+
+    // Инициализация региона захватываемого элемента из виртуальной сцены.
+    QRectF sourceRect;
+    QDeclarativeItem* targetItem = item->findChild<QDeclarativeItem*>(itemName);
+    if (targetItem) {
+        // Если в виртуальной сцене присутствует захватываемый элемент,
+        // то берется его относительный регион внутри сцены.
+        sourceRect = targetItem->sceneBoundingRect();
+    } else {
+        // Иначе берется регион из реальной сцены.
+        sourceRect = targetRect;
+    }
+
+
+    QImage img(targetRect.toRect().size(), QImage::Format_RGB32);
+    QPainter painter(&img);
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing |
+                           QPainter::SmoothPixmapTransform | QPainter::HighQualityAntialiasing);
+
+
+
     QGraphicsScene scene;
     scene.setBackgroundBrush(Qt::white);
     scene.addItem(item);
-    scene.render(&painter,  sourceItem->boundingRect(),  sourceItem->boundingRect().toRect());
+    scene.render(&painter, targetRect,  sourceRect);
     scene.removeItem(item);
     delete item;
     return img;
